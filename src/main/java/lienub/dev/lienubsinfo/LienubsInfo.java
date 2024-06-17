61cb80f66a61b1e362a4bb2e53f47601ac1f5030
Lienub <lienub.68@gmail.com>
2024-06-17 19:40:38 +0200

    Init project

---
+++ src/main/java/lienub/dev/lienubsinfo/LienubsInfo.java
@@ -0,0 +1,17 @@
+package lienub.dev.lienubsinfo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LienubsInfo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
